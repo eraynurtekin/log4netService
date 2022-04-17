35e033fb7de6ab97956ce477dd86d57e5737d9e0
Eray Nurtekin <79145084+eraynurtekin@users.noreply.github.com>
2022-04-17 15:30:49 +0300

    Add project files.

---
+++ log4netService/Program.cs
@@ -0,0 +1,36 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using log4net.Config;
+using Topshelf;
+
+namespace log4netService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            var exitCode = HostFactory.Run(hostConfigurator =>
+            {
+                hostConfigurator.SetServiceName("MyService");
+                hostConfigurator.SetDisplayName("My Service");
+                hostConfigurator.SetDescription("Does custom logic.");
+
+                hostConfigurator.RunAsLocalSystem();
+                hostConfigurator.UseLog4Net();
+
+                hostConfigurator.Service<log4net>(serviceConfigurator =>
+                {
+                    serviceConfigurator.ConstructUsing(() => new log4net());
+
+                    serviceConfigurator.WhenStarted(service => service.Start());
+                    serviceConfigurator.WhenStopped(service => service.Stop());
+                });
+            });
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ log4netService/log4net.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace log4netService
+{
+    public class log4net
+    {
+
+        private readonly Timer _timer;
+
+        public log4net()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+
+            File.AppendAllLines(@"C:\temp\my-windows-service\logs\log4net.txt", lines);
+        }
+
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+           
+        }
+    }
+}
